7d34377890183ce8d3ca958bb0f287aa3fbae33c
danil44 <dgolovenko0@gmail.com>
2020-04-11 15:28:41 +0300

    Updates

---
+++ precache-manifest.6e0c92a2e37a4d820e036d5d05166ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7016c14d1ecc274b8ba07892964540c0",
+    "url": "/vv/index.html"
+  },
+  {
+    "revision": "067d595207b67023baf1",
+    "url": "/vv/static/css/main.035d38f4.chunk.css"
+  },
+  {
+    "revision": "bd8e51c8b676d5edd089",
+    "url": "/vv/static/js/2.23ce95b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vv/static/js/2.23ce95b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067d595207b67023baf1",
+    "url": "/vv/static/js/main.5688ccbb.chunk.js"
+  },
+  {
+    "revision": "62018021d72c5e8c0ed1",
+    "url": "/vv/static/js/runtime-main.8c42fda5.js"
+  },
+  {
+    "revision": "ed3ad49b0bc6faaad8657ad75c156156",
+    "url": "/vv/static/media/right-arrow.ed3ad49b.svg"
+  }
+]);
